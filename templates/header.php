a82248dd28d03ef9945fa4816b67ffe0717c8a5e
Sete03 <sergiesbe@gmail.com>
2026-01-14 13:22:31 +0100

    Primera subida al repositorio

---
+++ templates/header.php
@@ -0,0 +1,23 @@
+<?php
+    include_once('datos.php');
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tartas Yoveia</title>
+    <link rel ="stylesheet" href="static/css/styles.css">
+</head>
+<body>
+    <header>
+        <div class = "centro">
+        <a href="inicio.php"><img class="logo" src="static/img/LogoPasteleria.png" alt="" height="120px"></a>
+        <ul>
+            <li><a href="inicio.php">INICIO</a></li>
+            <li><a href="inventario.php">TARTAS</a></li>
+            <li><a href="contacto.php">CONTACTO</a></li>
+            <li><a href="sesion.php">INICIAR SESION</a></li>
+        </ul>
+        </div>
+    </header>
