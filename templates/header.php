eb75ba7166ab448c0683c1eb8aaa352f9be7834f
Sete03 <sergiesbe@gmail.com>
2026-01-26 10:06:14 +0100

    Arreglo Favicon

--- templates/header.php
+++ templates/header.php
@@ -8,12 +8,12 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Tartas Yoveia</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
-    <link rel="icon" href="/static/img/faviconPasteleria.ico" type="image/x-icon">
+    <link rel="icon" type="image/png" href="static/img/faviconPasteleriaResize.png">
     <link rel ="stylesheet" href="static/css/stylesB.css">
 </head>
 <body>
     <header>
-        <div class = "centro">
+        <div class = "centro">            
             <a href="inicio.php"><img class="logo" src="static/img/LogoPasteleria.png" alt="" height="120px"></a>
         <ul>
             <li><a href="inicio.php">INICIO</a></li>
